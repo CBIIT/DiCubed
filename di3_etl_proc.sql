cb671f95ca44a5d0545029256d7d28b90e0de442
Hubert Hickman <hickmanhb@nih.gov>
2017-11-08 08:12:01 -0500

    spacing/prints

--- di3_etl_proc.sql
+++ di3_etl_proc.sql
@@ -5,12 +5,12 @@ LANGUAGE 'plpgsql'
 AS $body$
 BEGIN
 
-perform ispy_obs_fact_proc();
+select ispy_obs_fact_proc();
 delete from di3crcdata.observation_fact;
-perform load_fact_table('di3crcdata.observation_fact', 'ispy_obs_fact');
+select load_fact_table('di3crcdata.observation_fact', 'ispy_obs_fact');
 
 delete from di3crcdata.concept_dimension;
-perform load_concept_dim('di3metadata.di3');
+select load_concept_dim('di3metadata.di3');
 
 END;
 $body$
