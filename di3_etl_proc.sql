d0de824baa1070bb18fa2eeb29f9137a063b51a5
Hubert Hickman <hickmanhb@nih.gov>
2017-11-10 15:07:51 -0500

    Added text return to function

--- di3_etl_proc.sql
+++ di3_etl_proc.sql
@@ -1,16 +1,17 @@
 CREATE OR REPLACE FUNCTION di3_etl()
  
-RETURNS void 
-LANGUAGE 'plpgsql' 
+RETURNS text  
 AS $body$
+DECLARE ret_stuff text;
 BEGIN
 
-select ispy_obs_fact_proc();
+select ispy_obs_fact_proc() into ret_stuff;
 delete from di3crcdata.observation_fact;
-select load_fact_table('di3crcdata.observation_fact', 'ispy_obs_fact');
+select load_fact_table('di3crcdata.observation_fact', 'ispy_obs_fact') into ret_stuff;
 
 delete from di3crcdata.concept_dimension;
-select load_concept_dim('di3metadata.di3');
-
+select load_concept_dim('DI3') into ret_stuff;
+return ret_stuff;
 END;
 $body$
+LANGUAGE 'plpgsql' ;
