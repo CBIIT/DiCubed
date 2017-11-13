860eee10d5f7d19ea79c9a0c73368a65e87dc258
Hubert Hickman <hickmanhb@nih.gov>
2017-11-13 02:52:20 -0500

    Moved patient_num_seq to di3sources schema so it can be reset form di3sources owned functions

--- patient_mapping.sql
+++ patient_mapping.sql
@@ -7,12 +7,14 @@ BEGIN
 
 delete from di3crcdata.patient_mapping; 
 
+alter sequence patient_num_seq restart with 1;
+
 select nextval('di3crcdata.upload_status_upload_id_seq') into upload_id_v;
 
 insert into di3crcdata.patient_mapping(patient_ide, patient_ide_source, patient_num, patient_ide_status, project_id, upload_date, update_date, download_date, sourcesystem_cd, upload_id)
 select cast(d.subjectid as varchar) as patient_ide, 
        'i_spy_tcia_patient_clinical_subset' as patient_ide_source, 
-        nextval('di3crcdata.patient_num_seq') as patient_num, 
+        nextval('patient_num_seq') as patient_num, 
         'Active' as patient_ide_status, 
         'Dicubed' as project_id, 
        current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date, 
@@ -25,7 +27,7 @@ select nextval('di3crcdata.upload_status_upload_id_seq') into upload_id_v;
 insert into di3crcdata.patient_mapping(patient_ide, patient_ide_source, patient_num, patient_ide_status, project_id, upload_date, update_date, download_date, sourcesystem_cd, upload_id)
 select cast(d.breast_dx_case as varchar) as patient_ide, 
        'tcia_breast_clinical_data' as patient_ide_source, 
-        nextval('di3crcdata.patient_num_seq') as patient_num, 
+        nextval('patient_num_seq') as patient_num, 
         'Active' as patient_ide_status, 
         'Dicubed' as project_id, 
        current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date, 
