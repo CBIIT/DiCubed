81d99d0eaa44fb9f30549a60fe33e53a27fba193
Hubert Hickman <hickmanhb@nih.gov>
2017-11-13 02:27:28 -0500

    Added in tcia_breast_clincal_data to patient_mapping

--- patient_mapping.sql
+++ patient_mapping.sql
@@ -1,14 +1,37 @@
 
 
-CREATE OR REPLACE FUNCTION i_spy_patient_mapping ()
+CREATE OR REPLACE FUNCTION dicubed_patient_mapping ()
  RETURNS VOID AS $body$
+ DECLARE upload_id_v int;
 BEGIN
 
+delete from di3crcdata.patient_mapping; 
+
+select nextval('di3crcdata.upload_status_upload_id_seq') into upload_id_v;
+
 insert into di3crcdata.patient_mapping(patient_ide, patient_ide_source, patient_num, patient_ide_status, project_id, upload_date, update_date, download_date, sourcesystem_cd, upload_id)
-select cast(d.subjectid as varchar) as patient_ide, 'i_spy_tcia_patient_clinical_subset' as patient_ide_source, nextval('di3crcdata.patient_num_seq') as patient_num, 'Active' as patient_ide_status, 'Dicubed' as project_id, 
-       current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date, 'TCIA_ISPY1_Clinical' as sourcesystem_cd, nextval('di3crcdata.upload_status_upload_id_seq') as upload_id
+select cast(d.subjectid as varchar) as patient_ide, 
+       'i_spy_tcia_patient_clinical_subset' as patient_ide_source, 
+        nextval('di3crcdata.patient_num_seq') as patient_num, 
+        'Active' as patient_ide_status, 
+        'Dicubed' as project_id, 
+       current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date, 
+       'TCIA_ISPY1_Clinical' as sourcesystem_cd, 
+       upload_id_v  as upload_id
    from di3sources.i_spy_tcia_patient_clinical_subset d where d.subjectid is not null;
- 
+  
+select nextval('di3crcdata.upload_status_upload_id_seq') into upload_id_v;
+
+insert into di3crcdata.patient_mapping(patient_ide, patient_ide_source, patient_num, patient_ide_status, project_id, upload_date, update_date, download_date, sourcesystem_cd, upload_id)
+select cast(d.breast_dx_case as varchar) as patient_ide, 
+       'tcia_breast_clinical_data' as patient_ide_source, 
+        nextval('di3crcdata.patient_num_seq') as patient_num, 
+        'Active' as patient_ide_status, 
+        'Dicubed' as project_id, 
+       current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date, 
+       'TCIA_Breast-Diagnosis_Sheet1' as sourcesystem_cd, 
+       upload_id_v  as upload_id
+   from di3sources.tcia_breast_clinical_data d where d.breast_dx_case is not null;
 END;
 $body$
 LANGUAGE PLPGSQL;
