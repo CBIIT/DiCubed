9628540eae574e40a217fdb0fc4a53c51d8c583c
Hubert Hickman <hickmanhb@nih.gov>
2017-11-13 02:53:08 -0500

    Added tcia_breast_clinical_data encounters.  Cleaned up upload_id

--- encounter_mapping.sql
+++ encounter_mapping.sql
@@ -1,15 +1,40 @@
-
-
-CREATE OR REPLACE FUNCTION i_spy_encounter_mapping ()
+CREATE OR REPLACE FUNCTION dicubed_encounter_mapping ()
  RETURNS VOID AS $body$
+ DECLARE upload_id_v int;
 BEGIN
 
-insert into di3crcdata.encounter_mapping(encounter_ide, encounter_ide_source, project_id, encounter_num, patient_ide, patient_ide_source, encounter_ide_status, upload_date, download_date, import_date, sourcesystem_cd, upload_id)
-   select 'fabricated_for_' || d.subjectid as encounter_ide, 'i_spy_tcia_patient_clinical_subset' as encounter_ide_source, 'Dicubed' as project_id, nextval('di3sources.encounter_num_seq') as encounter_num, cast(d.subjectid as varchar) as patient_ide, 
-        'i_spy_tcia_patient_clinical_subset' as patient_ide_source, 'Active' as encounter_ide_status,  current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date,
-         'TCIA_ISPY1_Clinical' as sourcesystem_cd, 10 as upload_id 
+select nextval('di3crcdata.upload_status_upload_id_seq') into upload_id_v;
+
+alter sequence encounter_num_seq restart with 1;
+
+delete from di3crcdata.encounter_mapping;
+
+insert into di3crcdata.encounter_mapping(encounter_ide, encounter_ide_source, project_id, encounter_num, 
+                                         patient_ide, patient_ide_source, encounter_ide_status, upload_date, download_date, import_date, sourcesystem_cd, upload_id)
+        select 'fabricated_for_' || d.subjectid as encounter_ide, 
+        'i_spy_tcia_patient_clinical_subset' as encounter_ide_source, 
+        'Dicubed' as project_id,  
+         nextval('encounter_num_seq') as encounter_num, 
+         cast(d.subjectid as varchar) as patient_ide, 
+        'i_spy_tcia_patient_clinical_subset' as patient_ide_source, 
+         'Active' as encounter_ide_status,  current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date,
+         'TCIA_ISPY1_Clinical' as sourcesystem_cd, upload_id_v as upload_id 
 from di3sources.i_spy_tcia_patient_clinical_subset d where d.subjectid is not null;   
  
+select nextval('di3crcdata.upload_status_upload_id_seq') into upload_id_v;
+
+insert into di3crcdata.encounter_mapping(encounter_ide, encounter_ide_source, project_id, encounter_num, 
+                                         patient_ide, patient_ide_source, encounter_ide_status, upload_date, download_date, import_date, sourcesystem_cd, upload_id)
+        select 'fabricated_for_' || d.breast_dx_case as encounter_ide, 
+        'tcia_breast_clinical_data' as encounter_ide_source, 
+        'Dicubed' as project_id,  
+         nextval('encounter_num_seq') as encounter_num, 
+         cast(d.breast_dx_case as varchar) as patient_ide, 
+        'i_spy_tcia_patient_clinical_subset' as patient_ide_source, 
+         'Active' as encounter_ide_status,  current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date,
+         'TCIA_Breast-Diagnosis_Sheet1' as sourcesystem_cd, upload_id_v as upload_id 
+from di3sources.tcia_breast_clinical_data d where d.breast_dx_case is not null;   
+
 END;
 $body$
 LANGUAGE PLPGSQL;
