e1c4d3df72a697c5709f7189efb6e99864367d26
Hubert Hickman <hickmanhb@nih.gov>
2017-11-13 23:30:52 -0500

    Added Breast-MRI-NACT-Pilot data

--- di3_etl_proc.sql
+++ di3_etl_proc.sql
@@ -13,6 +13,9 @@ select load_fact_table('di3crcdata.observation_fact', 'ispy_obs_fact') into ret_
 select breast_diagnosis_obs_fact_proc() into ret_stuff;
 select load_fact_table('di3crcdata.observation_fact', 'breast_diagnosis_obs_fact') into ret_stuff;
 
+select shared_clinical_obs_fact_proc() into ret_stuff;
+select load_fact_table('di3crcdata.observation_fact', 'shared_clinical_obs_fact') into ret_stuff;
+
 delete from di3crcdata.concept_dimension;
 select load_concept_dim('DI3') into ret_stuff;
 return ret_stuff;
