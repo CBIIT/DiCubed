a74bdb26560c61db4abc88d2cb9c87af12cfe75c
Hubert Hickman <hickmanhb@nih.gov>
2017-11-13 20:48:12 -0500

    Added share_clinical_and_rfs data

--- encounter_mapping.sql
+++ encounter_mapping.sql
@@ -30,10 +30,24 @@ insert into di3crcdata.encounter_mapping(encounter_ide, encounter_ide_source, pr
         'Dicubed' as project_id,  
          nextval('encounter_num_seq') as encounter_num, 
          cast(d.breast_dx_case as varchar) as patient_ide, 
-        'i_spy_tcia_patient_clinical_subset' as patient_ide_source, 
+        'tcia_breast_clinical_data' as patient_ide_source, 
          'Active' as encounter_ide_status,  current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date,
          'TCIA_Breast-Diagnosis_Sheet1' as sourcesystem_cd, upload_id_v as upload_id 
 from di3sources.tcia_breast_clinical_data d where d.breast_dx_case is not null;   
+
+select nextval('di3crcdata.upload_status_upload_id_seq') into upload_id_v;
+
+insert into di3crcdata.encounter_mapping(encounter_ide, encounter_ide_source, project_id, encounter_num, 
+                                         patient_ide, patient_ide_source, encounter_ide_status, upload_date, download_date, import_date, sourcesystem_cd, upload_id)
+        select 'fabricated_for_' || d.patient_id as encounter_ide, 
+        'shared_clinical_and_rfs' as encounter_ide_source, 
+        'Dicubed' as project_id,  
+         nextval('encounter_num_seq') as encounter_num, 
+         cast(d.patient_id as varchar) as patient_ide, 
+        'shared_clinical_and_rfs' as patient_ide_source, 
+         'Active' as encounter_ide_status,  current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date,
+         'TCIA_Breast-MRI-NACT-Pilot_Clinical_and_RFS' as sourcesystem_cd, upload_id_v as upload_id 
+from di3sources.shared_clinical_and_rfs d where d.patient_id is not null;   
 
 END;
 $body$
