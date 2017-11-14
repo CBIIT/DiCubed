ac9dc26d19d8d6d5fa4935b1bad28dfa71582d95
Hubert Hickman <hickmanhb@nih.gov>
2017-11-14 02:52:37 -0500

    added some tcia_tcga data

--- di3_etl_proc.sql
+++ di3_etl_proc.sql
@@ -16,6 +16,9 @@ select load_fact_table('di3crcdata.observation_fact', 'breast_diagnosis_obs_fact
 select shared_clinical_obs_fact_proc() into ret_stuff;
 select load_fact_table('di3crcdata.observation_fact', 'shared_clinical_obs_fact') into ret_stuff;
 
+select tcia_tcga_obs_fact_proc() into ret_stuff;
+select load_fact_table('di3crcdata.observation_fact', 'tcia_tcga_obs_fact') into ret_stuff;
+
 delete from di3crcdata.concept_dimension;
 select load_concept_dim('DI3') into ret_stuff;
 return ret_stuff;
