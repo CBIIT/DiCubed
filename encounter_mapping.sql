558c96bb23a82defd4507137ab3d9d30efa2374f
Hubert Hickman <hickmanhb@nih.gov>
2017-11-29 18:16:18 -0500

    Added ivy gap patients

--- encounter_mapping.sql
+++ encounter_mapping.sql
@@ -77,6 +77,21 @@ insert into di3crcdata.encounter_mapping(encounter_ide, encounter_ide_source, pr
          'TCIA_TCGA-BRCA-follow_up_v4_0_BRCA' as sourcesystem_cd, upload_id_v as upload_id 
 from di3sources.nwc_org_clinical_follow_up_v4_0_brca   d where d.bcr_patient_barcode is not null;   
 
+select nextval('di3crcdata.upload_status_upload_id_seq') into upload_id_v;
+
+insert into di3crcdata.encounter_mapping(encounter_ide, encounter_ide_source, project_id, encounter_num, 
+                                         patient_ide, patient_ide_source, encounter_ide_status, upload_date, download_date, import_date, sourcesystem_cd, upload_id)
+with ivy_gap_pats as  (select distinct patient_id from ivy_report)
+        select 'fabricated_for_' || d.patient_id as encounter_ide, 
+        'ivy_report' as encounter_ide_source, 
+        'Dicubed' as project_id,  
+         nextval('encounter_num_seq') as encounter_num, 
+         cast(d.patient_id as varchar) as patient_ide, 
+        'ivy_report' as patient_ide_source, 
+         'Active' as encounter_ide_status,  current_timestamp as upload_date, current_timestamp as download_date, current_timestamp as update_date,
+         'IVY_GAP_ivy_report' as sourcesystem_cd, upload_id_v as upload_id 
+from ivy_gap_pats   d where d.patient_id is not null;   
+
 END;
 $body$
 LANGUAGE PLPGSQL;
