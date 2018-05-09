f3fb290f84b6f39e6cbb9d324d439af2e54defdd
Hubert Hickman <hickmanhb@nih.gov>
2018-05-09 05:32:18 -0400

    added in measures procedures

--- di3_etl_proc.sql
+++ di3_etl_proc.sql
@@ -28,13 +28,19 @@ select load_fact_table('di3crcdata.observation_fact', 'tcia_tcga_obs_fact') into
 select ivy_report_obs_fact_proc() into ret_stuff;
 select load_fact_table('di3crcdata.observation_fact', 'ivy_report_obs_fact') into ret_stuff; 
 
+select ucsf_measures_obs_fact_proc() into ret_stuff;
+select load_fact_table('di3crcdata.observation_fact', 'ucsf_measures_obs_fact') into ret_stuff; 
+
 delete from di3crcdata.patient_dimension;
 
 select load_patient_dimension() into ret_stuff;
 
+select load_visit_dimension() into ret_stuff;
+
 delete from di3crcdata.concept_dimension;
 select load_concept_dim('DI3') into ret_stuff;
 select load_concept_dim('NCI_THESAURUS') into ret_stuff;
+select load_concept_dim('MEASURES') into ret_stuff;
 return ret_stuff;
 END;
 $body$
